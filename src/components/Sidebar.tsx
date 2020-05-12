d2c405511f9bcb091de6dc8e82eafd8206ee3900
ndkhaivn <ndkhaivn@gmail.com>
2020-05-12 14:21:57 +0700

    Add Practitioner ID input

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import Navigation from './Navigation';
+import { InputGroup } from '@blueprintjs/core';
 
 export default function Sidebar() {
+
+  const [practitionerID, setPractitionerID] = useState("");
+
   return (
     <div className="sidebar">
       <Navigation />
+
+      <InputGroup
+          leftIcon="user"
+          onChange={(event: any) => setPractitionerID(event.target.value)}
+          placeholder="Practitioner ID"
+          value={practitionerID}
+      />
+
     </div>
   )
 }
