33e4baba7af0e787884813b157e41fe788792d21
ndkhaivn <ndkhaivn@gmail.com>
2020-05-24 14:57:24 +0700

    Only create a singleton of DataSource
    Previously creating multiple DataSource instances

--- src/store/practitioner/actions.ts
+++ src/store/practitioner/actions.ts
@@ -1,15 +1,17 @@
 import { ThunkAction } from 'redux-thunk';
 import { AnyAction } from "redux";
 import Practitioner from '../../DataModel/Practitioner';
-import DataSource from '../../DataModel/DataSource';
 import FHIRServer from '../../DataModel/FHIRServer';
 import { PractitionerActionTypes } from './types';
 
-const dataSource: DataSource = new FHIRServer();
-
 export const fetchPractitioner = (practitionerIdentifier: string): ThunkAction<void, {}, string, AnyAction> => (dispatch) => {
+
   dispatch({
-    type: PractitionerActionTypes.FETCH_DONE,
-    payload: new Practitioner(practitionerIdentifier)
+    type: PractitionerActionTypes.FETCH_REQUEST,
   });
+
+  // dispatch({
+  //   type: PractitionerActionTypes.FETCH_DONE,
+  //   payload: new Practitioner(practitionerIdentifier)
+  // });
 }
